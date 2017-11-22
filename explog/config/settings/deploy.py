346104843752e5ef58ea6cfc39edc5bf07a55566
Kyeong Lee <kaythechemist@gmail.com>
2017-11-22 16:41:03 +0900

    settings

---
+++ explog/config/settings/deploy.py
@@ -0,0 +1,31 @@
+from pprint import pprint
+
+from .base import *
+
+config_secret = json.loads(open(CONFIG_SECRET_DEV_FILE).read())
+DEBUG = False
+
+# AWS
+AWS_ACCESS_KEY_ID = config_secret["aws"]["acces_key_id"]
+AWS_SECRET_ACCESS_KEY = config_secret["aws"]["secret_access_key"]
+AWS_STORAGE_BUCKET_NAME = config_secret["aws"]["s3_bucket_name"]
+AWS_S3_SIGNATURE_VERSION = 's3v4'
+AWS_S3_REGION_NAME = 'ap-northeast-2'
+
+# AWS Storage
+STATICFILES_LOCATION = 'static'
+MEDIAFILES_LOCATION = 'media'
+
+# S3 FileStorage
+DEFAULT_FILE_STORAGE = 'config.storages.MediaStorage'
+STATICFILES_STORAGE = 'config.storages.StaticStorage'
+
+# Databases
+DATABASES = config_secret['django']['databases']
+
+# Allowed hosts
+ALLOWED_HOSTS = [
+    '.elasticbeanstalk.com',
+]
+
+pprint(DATABASES)
