59c7be90471330650a8d5bcf090b45c3db87b46e
zooozoo <shz0309@gmail.com>
2017-11-28 13:34:47 +0900

    login, signup 기능 구현 완성

--- explog/member/models.py
+++ explog/member/models.py
@@ -1,8 +1,8 @@
-from django.contrib.auth.base_user import AbstractBaseUser
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
-class User(AbstractBaseUser):
+class User(AbstractUser):
     img_profile = models.ImageField(
         upload_to='user',
         blank=True,
