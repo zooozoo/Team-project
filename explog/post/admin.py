4bc94fab0bb4431a74f5ef33e1f2abcc462b988c
Kangbok Lee <gangbok119@gmail.com>
2017-12-01 16:51:56 +0900

    model 수정 및 admin에 객체들 추가

--- explog/post/admin.py
+++ explog/post/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Post, PostContent, PostReply, PostText, PostPhoto, PostPath
+from .models import Post, PostContent, PostReply, PostText, PostPhoto, PostPath, PostPhotoGroup
 
 admin.site.register(Post)
 admin.site.register(PostContent)
@@ -9,3 +9,4 @@ admin.site.register(PostReply)
 admin.site.register(PostText)
 admin.site.register(PostPhoto)
 admin.site.register(PostPath)
+admin.site.register(PostPhotoGroup)
