483d7aaf8c0ea070ecf6bc84616c6b1fc94a3882
zooozoo <shz0309@gmail.com>
2017-12-04 18:30:03 +0900

    static dir  주석처리

--- explog/config/settings/base.py
+++ explog/config/settings/base.py
@@ -31,13 +31,13 @@ MEDIA_URL = '/media/'
 STATIC_ROOT = os.path.join(ROOT_DIR, '.static_root')
 
 # instagram_project/instagram/static/
-STATIC_DIR = os.path.join(BASE_DIR, 'static')
+# STATIC_DIR = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 # STATIC_URL로의 요청은 STATICFILES_DIRS경로의 목록에서 파일을 찾아 리턴
-STATICFILES_DIRS = [
-    STATIC_DIR,
-]
+# STATICFILES_DIRS = [
+#     STATIC_DIR,
+# ]
 # instagram_project/instagram/templates/
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 
