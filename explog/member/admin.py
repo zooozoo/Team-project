8d170951c6c3421f385a718b3ac617413b92feb7
zooozoo <shz0309@gmail.com>
2017-12-07 14:50:43 +0900

    어드민 수정

--- explog/member/admin.py
+++ explog/member/admin.py
@@ -4,6 +4,6 @@ from django.contrib.auth.admin import UserAdmin
 from member.models import User, Relation
 
 admin.site.register(User, UserAdmin)
-admin.site.register(Relation, UserAdmin)
+admin.site.register(Relation)
 
 # Register your models here.
